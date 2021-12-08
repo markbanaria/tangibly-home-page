5d7eff7f13b10f9c930d2116f0065056e6d39021
Mark Banaria <markbanaria@gmail.com>
2021-12-08 23:32:16 +0800

    video files

---
+++ video-player.js
@@ -0,0 +1,24 @@
+videoOpen = function (video) {
+  $('#main-video source').attr('src','assets/videos/' + video)
+  $('#modal-section').addClass('open')
+  a = $('#main-video')[0].play()
+  $('body').addClass('modal-open')
+}
+
+videoClose = function () {
+  $('body').removeClass('modal-open')
+  $('#modal-section').removeClass('open')
+  $('#main-video')[0].pause()
+  $('#main-video')[0].currentTime = 0
+}
+
+ctr=0
+
+setInterval(function(){
+  if(ctr==4) { ctr = 0 }
+  $( ".rolled" ).eq((ctr + 1) % 4).removeClass('rolled-over').removeClass('rolled-under')
+  $( ".rolled" ).eq((ctr + 2) % 4).removeClass('rolled-over')
+  $( ".rolled" ).eq((ctr + 3) % 4).removeClass('rolled-over').addClass('rolled-under')
+  $( ".rolled" ).eq(ctr).addClass('rolled-over')
+  ctr+=1
+}, 3000);
