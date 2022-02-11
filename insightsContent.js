8eebbd45eae1659b29b9b36eecb6cc3a4dce786b
Mark Banaria <markbanaria@gmail.com>
2022-02-12 00:27:53 +0800

    update navbar and add insights page

--- insightsContent.js
+++ insightsContent.js
@@ -5,7 +5,8 @@ var insightsContent = [
    body: "Introducing our CXO Case Law Reviews which are written to highlight issues and the impact across the C-Suite. \n This first case, Mallet v. Lacayo, is a really important decision that is just a few months old. It highlights the need to get your house in order if you want to enjoy the protection of trade secret laws.",
    channel: "linkedIn",
    tags: "#csuite #csuiteconversations #legaltech #legalops",
-   link: "https://www.linkedin.com/posts/tangiblyinc_tangibly-cxo-review-mallet-v-lacayo-activity-6892184605881982976-tidd" 
+   extlink: "https://www.linkedin.com/posts/tangiblyinc_tangibly-cxo-review-mallet-v-lacayo-activity-6892184605881982976-tidd" ,
+   link: "https://www.tangibly.com/0003"
   },
   {
    ID: 2,
@@ -13,7 +14,8 @@ var insightsContent = [
    body: "Every business has #tradesecrets - and not just technical know how. Trade secrets cut across the organization. Are you managing your trade secrets to a legally enforceable standard?",
    channel: "linkedIn",
    tags: "#tradesecrets",
-   link: "https://www.linkedin.com/posts/tangiblyinc_examples-of-trade-secrets-activity-6889973187136802816-STA-/" 
+   extlink: "https://www.linkedin.com/posts/tangiblyinc_examples-of-trade-secrets-activity-6889973187136802816-STA-/" ,
+   link: "https://www.tangibly.com/0002"
   },
   {
    ID: 3,
@@ -21,7 +23,8 @@ var insightsContent = [
    body: "Losses to businesses from the theft of their trade secrets add up to as much as three percent of the U.S. gross domestic product, according to a 2014 report by the Center for Responsible Enterprise and Trade and PricewaterhouseCoopers LLP...",
    channel: "linkedIn",
    tags: "",
-   link: "https://www.lawsitesblog.com/2022/02/launching-today-tangibly-is-first-platform-to-systematically-manage-a-companys-trade-secrets.html?utm_medium=social&utm_source=linkedin&utm_campaign=lawsitesblog-2022-02-03-39702" 
+   extlink: "https://www.lawsitesblog.com/2022/02/launching-today-tangibly-is-first-platform-to-systematically-manage-a-companys-trade-secrets.html?utm_medium=social&utm_source=linkedin&utm_campaign=lawsitesblog-2022-02-03-39702" ,
+   link: "https://www.tangibly.com/0004"
   },
   {
    ID: 4,
@@ -29,6 +32,16 @@ var insightsContent = [
    body: "Success is built on the back of a lot of trial and error. Understanding what does NOT work is an important and valuable asset for your company and can be protected as a trade secret. It’s called Negative Know-How.",
    channel: "linkedIn",
    tags: "#legaltech #tradesecrets #iplaw #intellectualproperty",
-   link: "www.google.com/" 
+   extlink: "www.google.com/" ,
+   link: "https://www.tangibly.com/0001"
+  },
+  {
+   ID: 5,
+   title: "Examples of Trade Secrets",
+   body: "Success is built on the back of a lot of trial and error. Understanding what does NOT work is an important and valuable asset for your company and can be protected as a trade secret. It’s called Negative Know-How.",
+   channel: "linkedIn",
+   tags: "#legaltech #tradesecrets #iplaw #intellectualproperty",
+   extlink: "www.google.com/" ,
+   link: "https://www.tangibly.com/0005"
   }
 ]
