a75d280654ab28181818ee23b6ab94f3052b16fa
Mark Banaria <markbanaria@gmail.com>
2022-02-12 00:33:54 +0800

    asdf

--- insightsContent.js
+++ insightsContent.js
@@ -6,7 +6,7 @@ var insightsContent = [
    channel: "linkedIn",
    tags: "#csuite #csuiteconversations #legaltech #legalops",
    extlink: "https://www.linkedin.com/posts/tangiblyinc_tangibly-cxo-review-mallet-v-lacayo-activity-6892184605881982976-tidd" ,
-   link: "https://www.tangibly.com/0003"
+   link: "https://www.tangibly.com/insights/0003"
   },
   {
    ID: 2,
@@ -15,7 +15,7 @@ var insightsContent = [
    channel: "linkedIn",
    tags: "#tradesecrets",
    extlink: "https://www.linkedin.com/posts/tangiblyinc_examples-of-trade-secrets-activity-6889973187136802816-STA-/" ,
-   link: "https://www.tangibly.com/0002"
+   link: "https://www.tangibly.com/insights/0002"
   },
   {
    ID: 3,
@@ -24,7 +24,7 @@ var insightsContent = [
    channel: "linkedIn",
    tags: "",
    extlink: "https://www.lawsitesblog.com/2022/02/launching-today-tangibly-is-first-platform-to-systematically-manage-a-companys-trade-secrets.html?utm_medium=social&utm_source=linkedin&utm_campaign=lawsitesblog-2022-02-03-39702" ,
-   link: "https://www.tangibly.com/0004"
+   link: "https://www.tangibly.com/insights/0004"
   },
   {
    ID: 4,
@@ -33,7 +33,7 @@ var insightsContent = [
    channel: "linkedIn",
    tags: "#legaltech #tradesecrets #iplaw #intellectualproperty",
    extlink: "www.google.com/" ,
-   link: "https://www.tangibly.com/0001"
+   link: "https://www.tangibly.com/insights/0001"
   },
   {
    ID: 5,
@@ -42,6 +42,6 @@ var insightsContent = [
    channel: "linkedIn",
    tags: "#legaltech #tradesecrets #iplaw #intellectualproperty",
    extlink: "www.google.com/" ,
-   link: "https://www.tangibly.com/0005"
+   link: "https://www.tangibly.com/insights/0005"
   }
 ]
