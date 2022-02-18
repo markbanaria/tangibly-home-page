61e00e07911a1fe70f4c89785f6d7b49e70c0d76
Mark Banaria <markbanaria@gmail.com>
2022-02-18 22:58:10 +0800

    add new videos

--- insightsContent.js
+++ insightsContent.js
@@ -1,47 +1,62 @@
 var insightsContent = [
   {
-   ID: 1,
-   title: "Trade secret case law reviews written for the C-suite",
-   body: "Introducing our CXO Case Law Reviews which are written to highlight issues and the impact across the C-Suite. \n This first case, Mallet v. Lacayo, is a really important decision that is just a few months old. It highlights the need to get your house in order if you want to enjoy the protection of trade secret laws.",
-   channel: "linkedIn",
-   tags: "#csuite #csuiteconversations #legaltech #legalops",
-   extlink: "https://www.linkedin.com/posts/tangiblyinc_tangibly-cxo-review-mallet-v-lacayo-activity-6892184605881982976-tidd" ,
-   link: "https://www.tangibly.com/insights/0003"
+   ID: 8,
+   date: 'Feb 1, 2020',
+   title: "Trade Secrets and Partnerships",
+   body: "How are you tracking the trade secrets you share with your development partners, supply chain, marketing partners and any other partners on the outside that help support your business?",
+   linkedin: "linkedIn",
+   tags: "#partnership #iplaw #intellectualproperty #tradesecrets #ip #supplychain",
+   extlink: "https://www.linkedin.com/feed/update/urn:li:activity:6899476565843955712?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A6899476565843955712%29" ,
+   link: "https://www.tangibly.com/videos/trade-secrets-partnerships"
   },
   {
-   ID: 2,
-   title: "Examples of Trade Secrets",
-   body: "Every business has #tradesecrets - and not just technical know how. Trade secrets cut across the organization. Are you managing your trade secrets to a legally enforceable standard?",
-   channel: "linkedIn",
-   tags: "#tradesecrets",
-   extlink: "https://www.linkedin.com/posts/tangiblyinc_examples-of-trade-secrets-activity-6889973187136802816-STA-/" ,
-   link: "https://www.tangibly.com/insights/0002"
+   ID: 9,
+   date: 'Feb 1, 2020',
+   title: "Case Study: EMC vs Stuart",
+   body: "Our second CXO Review has landed. Fascinating case where a company’s employment agreement did not adequately protect its trade secrets. What does the survival clause look like in your employment agreement??",
+   linkedin: "linkedIn",
+   tags: "#hr #iplaw #tradesecrets #employment #confidentiality",
+   extlink: "https://www.linkedin.com/feed/update/urn:li:activity:6897309277225140224?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A6897309277225140224%29" ,
+   link: "https://www.tangibly.com/insights/case-study-2"
+  },
+  {
+   ID: 4,
+   date: 'Feb 1, 2020',
+   title: "Negative Know How is a Trade Secret",
+   body: "Success is built on the back of a lot of trial and error. Understanding what does NOT work is an important and valuable asset for your company and can be protected as a trade secret. It’s called Negative Know-How.",
+   linkedin: "linkedIn",
+   tags: "#legaltech #tradesecrets #iplaw #intellectualproperty",
+   extlink: "https://www.linkedin.com/feed/update/urn:li:activity:6894054661905620992?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A6894054661905620992%29" ,
+   link: "https://www.tangibly.com/videos/negative-know-how-is-trade-secret"
   },
   {
    ID: 3,
+   date: 'Feb 1, 2020',
    title: "Launching Today, Tangibly is First Platform to Systematically Manage A Company’s Trade Secrets",
    body: "Losses to businesses from the theft of their trade secrets add up to as much as three percent of the U.S. gross domestic product, according to a 2014 report by the Center for Responsible Enterprise and Trade and PricewaterhouseCoopers LLP...",
-   channel: "linkedIn",
+   linkedin: "linkedIn",
    tags: "",
    extlink: "https://www.lawsitesblog.com/2022/02/launching-today-tangibly-is-first-platform-to-systematically-manage-a-companys-trade-secrets.html?utm_medium=social&utm_source=linkedin&utm_campaign=lawsitesblog-2022-02-03-39702" ,
    link: "https://www.tangibly.com/insights/0004"
   },
   {
-   ID: 4,
-   title: "Negative Know How is a Trade Secret",
-   body: "Success is built on the back of a lot of trial and error. Understanding what does NOT work is an important and valuable asset for your company and can be protected as a trade secret. It’s called Negative Know-How.",
-   channel: "linkedIn",
-   tags: "#legaltech #tradesecrets #iplaw #intellectualproperty",
-   extlink: "www.google.com/" ,
-   link: "https://www.tangibly.com/insights/0001"
+   ID: 2,
+   date: 'Feb 1, 2020',
+   title: "Examples of Trade Secrets",
+   body: "Every business has #tradesecrets - and not just technical know how. Trade secrets cut across the organization. Are you managing your trade secrets to a legally enforceable standard?",
+   linkedin: "linkedIn",
+   tags: "#tradesecrets",
+   extlink: "https://www.linkedin.com/posts/tangiblyinc_examples-of-trade-secrets-activity-6889973187136802816-STA-/" ,
+   link: "https://www.tangibly.com/videos/examples-of-trade-secrets"
   },
   {
-   ID: 8,
-   title: "Trade Secrets and Partnerships",
-   body: "How are you tracking the trade secrets you share with your development partners, supply chain, marketing partners and any other partners on the outside that help support your business?",
-   channel: "linkedIn",
-   tags: "#partnership #iplaw #intellectualproperty #tradesecrets #ip #supplychain",
-   extlink: "www.google.com/" ,
-   link: "https://www.tangibly.com/insights/0008"
-  }
+   ID: 1,
+   date: 'Feb 1, 2020',
+   title: "Trade secret case law reviews written for the C-suite",
+   body: "Introducing our CXO Case Law Reviews which are written to highlight issues and the impact across the C-Suite. \n This first case, Mallet v. Lacayo, is a really important decision that is just a few months old. It highlights the need to get your house in order if you want to enjoy the protection of trade secret laws.",
+   linkedin: "linkedIn",
+   tags: "#csuite #csuiteconversations #legaltech #legalops",
+   extlink: "https://www.linkedin.com/posts/tangiblyinc_tangibly-cxo-review-mallet-v-lacayo-activity-6892184605881982976-tidd" ,
+   link: "https://www.tangibly.com/insights/case-study-1"
+  },
 ]
