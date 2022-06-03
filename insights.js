7ca5156549e9a027968c668d86399b74f4adb7c6
Mark Banaria <markbanaria@gmail.com>
2022-06-04 02:47:42 +0800

    add new articles

--- insights.js
+++ insights.js
@@ -1,10 +1,17 @@
 function generateInsights(howMany) {
   var b = ''
   var c = 0
+  var sourceIcon = ''
   if(howMany=='all') { c = insightsContent.length } 
   else { c = 3 }
   for(i=0;i<c;i++) {
-    a = '<div class="col-12 mt24"> <div class="box-shadow-lg border-radius-md container-fluid"> <div class="row"> <div class="col-xs-12 col-md-6 col-lg-4" style="min-height: 180px; background-image: url(\'assets/images/articles/' + insightsContent[i].ID + '.png\'); background-color: #dcdcdc; background-size: contain; background-position: 50% 50%; background-repeat: no-repeat"> </div><div class="col-xs-12 col-md-6 col-lg-8"> <div class="p32 xs-p24"> <div class="d-flex flex-row"> <a href="' + insightsContent[i].link + '" class=" flex-fill font-24 mb16 display-block text-copy-2"> <strong>' + insightsContent[i].title + ' </strong> </a> <a href="' + insightsContent[i].extlink + '" class="' + insightsContent[i].linkedin + '"> <img src="assets/images/linked-in-icon-blue.svg"></a> </div><div>' + insightsContent[i].body + '<br><br>' + insightsContent[i].tags + ' </div></div></div></div></div></div>'
+
+    if (insightsContent[i].source == 'linkedIn') {sourceIcon = 'linked-in-icon-blue'}
+    else if (insightsContent[i].source == 'web') {sourceIcon = 'web-link-icon'}
+    else if (insightsContent[i].source == 'none') {sourceIcon = 'web-link-icon'}
+    else {}
+
+    a = '<div class="col-12 mt24"> <div class="box-shadow-lg border-radius-md container-fluid"> <div class="row"> <div class="col-xs-12 col-md-6 col-lg-4" style="min-height: 180px; background-image: url(\'assets/images/articles/' + insightsContent[i].ID + '.png\'); background-color: #dcdcdc; background-size: contain; background-position: 50% 50%; background-repeat: no-repeat"> </div><div class="col-xs-12 col-md-6 col-lg-8"> <div class="p32 xs-p24"> <div class="d-flex flex-row"> <a href="' + insightsContent[i].link + '" class=" flex-fill font-24 mb16 display-block text-copy-2"> <strong>' + insightsContent[i].title + ' </strong> </a> <a href="' + insightsContent[i].extlink + '" class="' + insightsContent[i].linkedin + '"> <img src="assets/images/' + sourceIcon + '.svg" class="pl24"></a> </div><div>' + insightsContent[i].body + '<br><br>' + insightsContent[i].tags + ' </div></div></div></div></div></div>'
     b = b + a
   }
   document.getElementById('insights-container').innerHTML = b
--- insightsContent.js
+++ insightsContent.js
@@ -1,10 +1,90 @@
 var insightsContent = [
   {
+   ID: 18,
+   date: 'May 20, 2022',
+   title: " $2B in damages awarded in Appian v PegaSystems",
+   body: "I've been a little behind the curve on LinkedIN posting the last few weeks as we're knee deep in onboarding customers and building the coolest trade secret tool the world has seen...",
+   source: "linkedIn",
+   tags: "#intellectualproperty #iplaw #ip",
+   extlink: "https://www.linkedin.com/posts/timlondergan_appian-awarded-2036-billion-in-damages-activity-6933125537703886848-vdLE/?utm_source=linkedin_share&utm_medium=member_desktop_web" ,
+   link: "https://www.tangibly.com/insights/timlondergan_appian-awarded-2036-billion-in-damages-activity"
+  },
+  {
+   ID: 17,
+   date: 'May 30, 2022',
+   title: "The Economist: Corporate espionage is entering a new era",
+   body: "For espionage of the cloak-and-dagger variety, it is hard to beat John le Carré or Ian Fleming. But the world of corporate spying has plenty of drama, too...",
+   source: "web",
+   tags: "",
+   extlink: "https://www.economist.com/business/2022/05/30/corporate-espionage-is-entering-a-new-era" ,
+   link: "https://www.tangibly.com/insights/corporate-espionage-is-entering-a-new-era"
+  },
+  {
+   ID: 16,
+   date: 'Mar 1, 2022',
+   title: "IP Office of Singapore Releases Report on Trade Secrets",
+   body: "<to follow>",
+   source: "linkedIn",
+   tags: "#intellectualproperty #iplaw #ip",
+   extlink: "https://www.linkedin.com/posts/timlondergan_protection-and-management-of-trade-secrets-activity-6904664996924522496-UJEV/?utm_source=linkedin_share&utm_medium=member_desktop_web" ,
+   link: "https://www.tangibly.com/insights/timlondergan_protection-and-management-of-trade-secrets-activity"
+  },
+  {
+   ID: 15,
+   date: 'Apr 25, 2022',
+   title: "Trade secrets and China - Tangibly mentioned by Rouse",
+   body: "Trade secrets are boring..until something goes wrong...",
+   source: "linkedIn",
+   tags: "#tradesecrets #china #iplitigation",
+   extlink: "https://www.linkedin.com/posts/chrisbaileychina_rouse-launches-china-trade-secret-litigation-activity-6924411629363245057-9t0u/?utm_source=linkedin_share&utm_medium=member_desktop_web" ,
+   link: "https://www.tangibly.com/insights/chrisbaileychina_rouse-launches-china-trade-secret-litigation-activity"
+  },
+  {
+   ID: 14,
+   date: 'Jan 19, 2022',
+   title: "Taiwan’s TSMC – A Trade Secret Cult(ure)",
+   body: "Trade secrets are emerging as an asset class of growing importance. Some of the quintessential trade secret examples are the Coca-Cola recipe, the WD-40 formulation, and my personal favorite, the Colonel’s secret recipe (KFC). Beyond these easily recognizable examples lie millions of trade secrets that drive innovation, competitive advantage, and ultimately, profitability for companies both large and small...",
+   source: "linkedIn",
+   tags: "#tradesecrets #iplaw #intellectualproperty #patents #innovation #semiconductor #legaltech #intangibleassets",
+   extlink: "https://www.linkedin.com/pulse/taiwans-tsmc-trade-secret-culture-tim-londergan" ,
+   link: "https://www.tangibly.com/insights/taiwans-tsmc-trade-secret-culture-tim-londergan"
+  },
+  {
+   ID: 13,
+   date: 'Feb 1, 2020',
+   title: "CXO Review 3: Amgen v CCH",
+   body: "Introducing our CXO Case Law Reviews which are written to highlight issues and the impact across the C-Suite. \n This first case, Mallet v. Lacayo, is a really important decision that is just a few months old. It highlights the need to get your house in order if you want to enjoy the protection of trade secret laws.",
+   source: "none",
+   tags: "",
+   extlink: "https://www.tangibly.com/insights/case-study-3" ,
+   link: "https://www.tangibly.com/insights/case-study-3"
+  },
+  {
+   ID: 12,
+   date: 'Mar 18, 2022',
+   title: "The trade secret skills gap and why firms must close it: in-house",
+   body: "Attorneys at Relativity Space, Honeywell and Lam Research say there’s a greater demand than supply for lawyers who can provide day-to-day trade secret advisory services",
+   source: "web",
+   tags: "",
+   extlink: "https://www.managingip.com/article/b1x715x2r0p4zj/the-trade-secret-skills-gap-and-why-firms-must-close-it-in-house" ,
+   link: "https://www.tangibly.com/insights/the-trade-secret-skills-gap-and-why-firms-must-close-it-in-house"
+  },
+  {
+   ID: 11,
+   date: 'Apr 1, 2022',
+   title: "Trade secret protection pitfalls: how counsel get trapped",
+   body: "Trade secret protection pitfalls: how counsel get trappedAttorneys explain why companies must be careful when documenting trade secrets, outlining employee agreements and conducting trainings",
+   source: "web",
+   tags: "",
+   extlink: "https://www.managingip.com/article/b1xds188ptfsyn/trade-secret-protection-pitfalls-how-counsel-get-trapped" ,
+   link: "https://www.tangibly.com/insights/trade-secret-protection-pitfalls-how-counsel-get-trapped"
+  },
+  {
    ID: 10,
    date: 'Feb 1, 2020',
    title: "Rapid Innovation Is Driving Demand For Trade Secrets",
    body: "What do patents and free lunch have in common?? ✓ They are both strategic assets that help companies maintain a competitive edge. ✓ But unlike a free lunch that helps recruit and retain talent, patent portfolios of leading companies are like a big heavy hammer always at the ready to assert against your competition...",
-   linkedin: "linkedIn",
+   source: "linkedIn",
    tags: "#innovation #ip #tradesecrets #intellectualproperty #patents",
    extlink: "https://www.linkedin.com/feed/update/urn:li:activity:6902129829223763968/" ,
    link: "https://www.tangibly.com/insights/rapid-innovation-is-driving-demand-for-trade-secrets"
@@ -14,7 +94,7 @@ var insightsContent = [
    date: 'Feb 1, 2020',
    title: "Trade Secrets and Partnerships",
    body: "How are you tracking the trade secrets you share with your development partners, supply chain, marketing partners and any other partners on the outside that help support your business?",
-   linkedin: "linkedIn",
+   source: "linkedIn",
    tags: "#partnership #iplaw #intellectualproperty #tradesecrets #ip #supplychain",
    extlink: "https://www.linkedin.com/feed/update/urn:li:activity:6899476565843955712?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A6899476565843955712%29" ,
    link: "https://www.tangibly.com/videos/trade-secrets-partnerships"
@@ -24,7 +104,7 @@ var insightsContent = [
    date: 'Feb 1, 2020',
    title: "Case Study: EMC vs Stuart",
    body: "Our second CXO Review has landed. Fascinating case where a company’s employment agreement did not adequately protect its trade secrets. What does the survival clause look like in your employment agreement??",
-   linkedin: "linkedIn",
+   source: "linkedIn",
    tags: "#hr #iplaw #tradesecrets #employment #confidentiality",
    extlink: "https://www.linkedin.com/feed/update/urn:li:activity:6897309277225140224?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A6897309277225140224%29" ,
    link: "https://www.tangibly.com/insights/case-study-2"
@@ -34,7 +114,7 @@ var insightsContent = [
    date: 'Feb 1, 2020',
    title: "Negative Know How is a Trade Secret",
    body: "Success is built on the back of a lot of trial and error. Understanding what does NOT work is an important and valuable asset for your company and can be protected as a trade secret. It’s called Negative Know-How.",
-   linkedin: "linkedIn",
+   source: "linkedIn",
    tags: "#legaltech #tradesecrets #iplaw #intellectualproperty",
    extlink: "https://www.linkedin.com/feed/update/urn:li:activity:6894054661905620992?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A6894054661905620992%29" ,
    link: "https://www.tangibly.com/videos/negative-know-how-is-trade-secret"
@@ -44,7 +124,7 @@ var insightsContent = [
    date: 'Feb 1, 2020',
    title: "Launching Today, Tangibly is First Platform to Systematically Manage A Company’s Trade Secrets",
    body: "Losses to businesses from the theft of their trade secrets add up to as much as three percent of the U.S. gross domestic product, according to a 2014 report by the Center for Responsible Enterprise and Trade and PricewaterhouseCoopers LLP...",
-   linkedin: "linkedIn",
+   source: "web",
    tags: "",
    extlink: "https://www.lawsitesblog.com/2022/02/launching-today-tangibly-is-first-platform-to-systematically-manage-a-companys-trade-secrets.html?utm_medium=social&utm_source=linkedin&utm_campaign=lawsitesblog-2022-02-03-39702" ,
    link: "https://www.tangibly.com/insights/0004"
@@ -54,7 +134,7 @@ var insightsContent = [
    date: 'Feb 1, 2020',
    title: "Examples of Trade Secrets",
    body: "Every business has #tradesecrets - and not just technical know how. Trade secrets cut across the organization. Are you managing your trade secrets to a legally enforceable standard?",
-   linkedin: "linkedIn",
+   source: "linkedIn",
    tags: "#tradesecrets",
    extlink: "https://www.linkedin.com/posts/tangiblyinc_examples-of-trade-secrets-activity-6889973187136802816-STA-/" ,
    link: "https://www.tangibly.com/videos/examples-of-trade-secrets"
@@ -64,9 +144,9 @@ var insightsContent = [
    date: 'Feb 1, 2020',
    title: "Trade secret case law reviews written for the C-suite",
    body: "Introducing our CXO Case Law Reviews which are written to highlight issues and the impact across the C-Suite. \n This first case, Mallet v. Lacayo, is a really important decision that is just a few months old. It highlights the need to get your house in order if you want to enjoy the protection of trade secret laws.",
-   linkedin: "linkedIn",
+   source: "linkedIn",
    tags: "#csuite #csuiteconversations #legaltech #legalops",
    extlink: "https://www.linkedin.com/posts/tangiblyinc_tangibly-cxo-review-mallet-v-lacayo-activity-6892184605881982976-tidd" ,
    link: "https://www.tangibly.com/insights/case-study-1"
-  },
+  }
 ]
